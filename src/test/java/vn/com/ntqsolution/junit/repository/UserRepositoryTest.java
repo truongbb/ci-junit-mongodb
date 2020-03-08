3f0140124369ccb572ff7172b47dbaf9a166fcc1
truongbb <truongbb96@outlook.com>
2020-03-09 00:16:46 +0700

    fix(test repo): fix unit test repository

--- src/test/java/vn/com/ntqsolution/junit/repository/UserRepositoryTest.java
+++ src/test/java/vn/com/ntqsolution/junit/repository/UserRepositoryTest.java
@@ -3,18 +3,18 @@ package vn.com.ntqsolution.junit.repository;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 import vn.com.ntqsolution.junit.repository.user.UserRepositoryJpa;
 
 @DataMongoTest
+@ComponentScan
 @RunWith(SpringRunner.class)
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@ActiveProfiles("unit-test")
 public class UserRepositoryTest {
 
     @Autowired
