c4aa67bc281914596c85f22efad88eed848a9281
truongbb <truongbb96@outlook.com>
2020-03-12 23:59:26 +0700

    (chore): commit some notes

--- src/test/java/vn/com/ntqsolution/junit/JunitApplicationTests.java
+++ src/test/java/vn/com/ntqsolution/junit/JunitApplicationTests.java
@@ -10,4 +10,21 @@ class JunitApplicationTests {
     void contextLoads() {
     }
 
+  /**
+   * @see
+   *
+   * 1. https://kipalog.com/posts/Spring-Boot--18--Hu-o--ng-da--n-chi-tie--t-Test-Spring-Boot
+   * 2. https://loda.me/test-huong-dan-toan-tap-mockito-loda1576641016810/
+   * 3. https://www.baeldung.com/spring-boot-testing
+   * 4. https://github.com/ti3r/spring-data-mongodb-test
+   * 5. https://github.com/maiconpintoabreu/Spring5SecurityMongodbTest (with authen)
+   *
+   *
+   * Embedded mongo: http://www.zakariaamine.com/2018-11-11/testing-spring-custom-mongo-port
+   *
+   *
+   *
+   */
+
+
 }
